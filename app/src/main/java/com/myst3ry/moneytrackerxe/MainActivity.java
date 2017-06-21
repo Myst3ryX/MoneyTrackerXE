394f0dcef5cbef3fca6178ac1d485e0462661dec
Alexey Egorov <alex.myst3ry@gmail.com>
2017-06-22 02:24:38 +0300

    Created new screen design in activity_main.xml: added and styled view-components. Added listeners on text input in MainActivity.java

--- app/src/main/java/com/myst3ry/moneytrackerxe/MainActivity.java
+++ app/src/main/java/com/myst3ry/moneytrackerxe/MainActivity.java
@@ -2,6 +2,10 @@ package com.myst3ry.moneytrackerxe;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +13,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView addButton = (TextView) findViewById(R.id.addButton);
+        final EditText articleName = (EditText) findViewById(R.id.articleName);
+        final EditText priceField = (EditText) findViewById(R.id.priceField);
+        TextWatcher watcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (!articleName.getText().toString().matches("") && !priceField.getText().toString().matches("")) {
+                    addButton.setEnabled(true);
+                } else {
+                    addButton.setEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        };
+
+        articleName.addTextChangedListener(watcher);
+        priceField.addTextChangedListener(watcher);
     }
 
     @Override
