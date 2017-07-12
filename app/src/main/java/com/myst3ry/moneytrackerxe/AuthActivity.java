520d15a3925d2cf3f0efff4164183231a150b02b
Alexey Egorov <alex.myst3ry@gmail.com>
2017-07-12 12:47:25 +0300

    Fixed View elements disposition to RelativeLayout in activity_auth.xml.

--- app/src/main/java/com/myst3ry/moneytrackerxe/AuthActivity.java
+++ app/src/main/java/com/myst3ry/moneytrackerxe/AuthActivity.java
@@ -81,7 +81,6 @@ public class AuthActivity extends AppCompatActivity {
                         if (data != null && data.isSuccessful()) {
                             ((LSApp) getApplication()).editAuthToken(data.getAuthToken());
                             startActivity(new Intent(AuthActivity.this, MainActivity.class));
-                            finish();
                         } else {
                             Toast.makeText(AuthActivity.this, getString(R.string.auth_error, data.getStatus()), Toast.LENGTH_SHORT).show();
                         }
